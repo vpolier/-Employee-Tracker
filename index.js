29652c6a138d326dcd8c6c7ccfd312d4c76e63f2
vpolier <111066480+vpolier@users.noreply.github.com>
2023-03-23 11:42:42 +1000

    function to view employee by department

--- index.js
+++ index.js
@@ -478,3 +478,51 @@ updateManager = () => {
     });
   });
 };
+
+// function to view employee by department
+employeeDepartment = () => {
+  console.log('Showing employee by departments...\n');
+  const sql = `SELECT employee.first_name, 
+                      employee.last_name, 
+                      department.name AS department
+               FROM employee 
+               LEFT JOIN role ON employee.role_id = role.id 
+               LEFT JOIN department ON role.department_id = department.id`;
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err; 
+    console.table(rows); 
+    promptUser();
+  });          
+};
+
+// function to delete department
+deleteDepartment = () => {
+  const deptSql = `SELECT * FROM department`; 
+
+  connection.promise().query(deptSql, (err, data) => {
+    if (err) throw err; 
+
+    const dept = data.map(({ name, id }) => ({ name: name, value: id }));
+
+    inquirer.prompt([
+      {
+        type: 'list', 
+        name: 'dept',
+        message: "What department do you want to delete?",
+        choices: dept
+      }
+    ])
+      .then(deptChoice => {
+        const dept = deptChoice.dept;
+        const sql = `DELETE FROM department WHERE id = ?`;
+
+        connection.query(sql, dept, (err, result) => {
+          if (err) throw err;
+          console.log("Successfully deleted!"); 
+
+        showDepartments();
+      });
+    });
+  });
+};
