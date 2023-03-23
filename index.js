dab058a7a905dfcc5d413a779f86e6eead5ab586
vpolier <111066480+vpolier@users.noreply.github.com>
2023-03-23 11:37:04 +1000

    grab roles from table

--- index.js
+++ index.js
@@ -295,3 +295,60 @@ addEmployee = () => {
   ])
     .then(answer => {
     const params = [answer.fistName, answer.lastName]
+
+  // grab roles from roles table
+  const roleSql = `SELECT role.id, role.title FROM role`;
+  
+  connection.promise().query(roleSql, (err, data) => {
+    if (err) throw err; 
+    
+    const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+
+    inquirer.prompt([
+          {
+            type: 'list',
+            name: 'role',
+            message: "What is the employee's role?",
+            choices: roles
+          }
+        ])
+          .then(roleChoice => {
+            const role = roleChoice.role;
+            params.push(role);
+
+            const managerSql = `SELECT * FROM employee`;
+
+            connection.promise().query(managerSql, (err, data) => {
+              if (err) throw err;
+
+              const managers = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+
+              // console.log(managers);
+
+              inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'manager',
+                  message: "Who is the employee's manager?",
+                  choices: managers
+                }
+              ])
+                .then(managerChoice => {
+                  const manager = managerChoice.manager;
+                  params.push(manager);
+
+                  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                  VALUES (?, ?, ?, ?)`;
+
+                  connection.query(sql, params, (err, result) => {
+                  if (err) throw err;
+                  console.log("Employee has been added!")
+
+                  showEmployees();
+            });
+          });
+        });
+      });
+   });
+});
+};
