9d889d460af9899dc86decac04cf43dbe1e6999d
vpolier <111066480+vpolier@users.noreply.github.com>
2023-03-23 11:43:56 +1000

    function to delete role

--- index.js
+++ index.js
@@ -526,3 +526,34 @@ deleteDepartment = () => {
     });
   });
 };
+
+// function to delete role
+deleteRole = () => {
+  const roleSql = `SELECT * FROM role`; 
+
+  connection.promise().query(roleSql, (err, data) => {
+    if (err) throw err; 
+
+    const role = data.map(({ title, id }) => ({ name: title, value: id }));
+
+    inquirer.prompt([
+      {
+        type: 'list', 
+        name: 'role',
+        message: "What role do you want to delete?",
+        choices: role
+      }
+    ])
+      .then(roleChoice => {
+        const role = roleChoice.role;
+        const sql = `DELETE FROM role WHERE id = ?`;
+
+        connection.query(sql, role, (err, result) => {
+          if (err) throw err;
+          console.log("Successfully deleted!"); 
+
+          showRoles();
+      });
+    });
+  });
+};
