baa501bcc51e16ea706fb40c5b217b99b070b26d
vpolier <111066480+vpolier@users.noreply.github.com>
2023-03-23 11:26:30 +1000

    function to add a department

--- index.js
+++ index.js
@@ -167,3 +167,32 @@ showEmployees = () => {
     promptUser();
   });
 };
+
+// function to add a department 
+addDepartment = () => {
+  inquirer.prompt([
+    {
+      type: 'input', 
+      name: 'addDept',
+      message: "What department do you want to add?",
+      validate: addDept => {
+        if (addDept) {
+            return true;
+        } else {
+            console.log('Please enter a department');
+            return false;
+        }
+      }
+    }
+  ])
+    .then(answer => {
+      const sql = `INSERT INTO department (name)
+                  VALUES (?)`;
+      connection.query(sql, answer.addDept, (err, result) => {
+        if (err) throw err;
+        console.log('Added ' + answer.addDept + " to departments!"); 
+
+        showDepartments();
+    });
+  });
+};
