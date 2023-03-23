24b843694274160e0c2bcaedf1d69f32703d750e
vpolier <111066480+vpolier@users.noreply.github.com>
2023-03-23 11:40:58 +1000

    function to update Manager

--- index.js
+++ index.js
@@ -415,3 +415,66 @@ updateEmployee = () => {
     });
   });
 };
+
+// function to update Manager
+updateManager = () => {
+  // get employees from employee table 
+  const employeeSql = `SELECT * FROM employee`;
+
+  connection.promise().query(employeeSql, (err, data) => {
+    if (err) throw err; 
+
+  const employees = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'name',
+        message: "Which employee would you like to update?",
+        choices: employees
+      }
+    ])
+      .then(empChoice => {
+        const employee = empChoice.name;
+        const params = []; 
+        params.push(employee);
+
+        const managerSql = `SELECT * FROM employee`;
+
+          connection.promise().query(managerSql, (err, data) => {
+            if (err) throw err; 
+
+          const managers = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+            
+              inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'manager',
+                  message: "Who is the employee's manager?",
+                  choices: managers
+                }
+              ])
+                  .then(managerChoice => {
+                    const manager = managerChoice.manager;
+                    params.push(manager); 
+                    
+                    let employee = params[0]
+                    params[0] = manager
+                    params[1] = employee 
+                    
+
+                    // console.log(params)
+
+                    const sql = `UPDATE employee SET manager_id = ? WHERE id = ?`;
+
+                    connection.query(sql, params, (err, result) => {
+                      if (err) throw err;
+                    console.log("Employee has been updated!");
+                  
+                    showEmployees();
+          });
+        });
+      });
+    });
+  });
+};
